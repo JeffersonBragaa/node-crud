0008b13e43e4001b91a338dde9044c9a7e0ab3be
jefferson <jekonth@gmail.com>
2026-04-06 00:00:17 -0300

    acho que fim

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
+const { cadastrarProd } = require('./db')
+const { mostraProdutosAll } = require('./db')
+const { deletaProduto } = require('./db')
+const { mostraProdutoId } = require('./db')
+const { venderProduto } = require('./db')
+const { atualizaProduto } = require('./db')
+
 const express = require('express');
 
 const app = express();
@@ -6,93 +13,177 @@ app.use(express.json());
 let produtos = [];
 
 
-app.get('/produtos-cadastrados', (req, res) => {
-  res.json(produtos);
+app.get('/produtos-cadastrados', async (req, res) => {
+    try {
+        const produtos = await mostraProdutosAll();
+
+        return res.status(200).json({
+            mensagem: 'Produtos cadastrados',
+            produtos: produtos
+        });
+
+    } catch (erro) {
+        
+        if(erro.message === 'NADA_CADASTRADO'){
+            return res.status(404).json({
+                mensagem: 'Nenhum produto cadastrado no sistema'
+            });
+        }
+    
+        return res.status(500).json({ mensagem: 'Erro Interno' });
+    }
 });
 
 
-app.get('/produto/:id', (req, res) => {
+app.get('/produto/:id', async (req, res) => {
     const id = Number(req.params.id);
-    const produto = produtos.find(p => p.id === id);
+    
+    try {
+        const produto= await mostraProdutoId(id);
 
-    if (produto) {
-        return res.json(produto);
-    } 
+        return res.status(200).json({
+            mensagem: 'Produto Encontrado',
+            produtos: produto
+        });
 
-    return res.status(404).json({ mensagem: 'Produto não encontrado' });
+    } catch (erro) {
+        if(erro.message === 'PROD_NAO_ENCONTRADO'){
+            return res.status(404).json({
+                menssagem: 'Produto não encontrado'
+            })
+        }
+        console.error(erro);
+        return res.status(500).json({ mensagem: 'Erro ao buscar produto' });
+    }
 });
 
 
-app.post('/cadastrar-produto', (req, res) => {
-    const { id, nome, preco, quantidade } = req.body;
+app.post('/cadastrar-produto', async (req, res) => {
+    const {nome, preco, quantidade } = req.body;
 
-    if (!id || !nome || !preco || !quantidade) {
+    if (!nome || !preco || !quantidade) {
         return res.status(400).json({ mensagem: 'Dados inválidos' });
     }
 
     const prod = {
-        id: Number(id),
         nome,
         preco: Number(preco),
         quantidade: Number(quantidade)
     };
 
-    const existente = produtos.find(p => p.id === prod.id);
+    try {
+        const produtoCadastrado = await cadastrarProd(prod);
 
-    if (existente) {
-        existente.quantidade += prod.quantidade;
-        return res.json({ mensagem: 'Produto atualizado com sucesso', produto: existente });
+        return res.status(201).json({
+            mensagem: 'Produto cadastrado com sucesso',
+            produto: produtoCadastrado
+        });
+
+    } catch (erro) {
+        if(erro.message === 'PROD_JA_CADASTRADO'){
+            return res.status(400).json({ mensagem: 'Já existe um produto com esse nome' });
+        }
+        console.error(erro);
+        return res.status(500).json({ mensagem: 'Erro ao cadastrar produto' });
     }
-
-    produtos.push(prod);
-    return res.status(201).json({ mensagem: 'Produto cadastrado com sucesso', produto: prod });
 });
 
-app.post('/vender-produto', (req, res) => {
-    const { id, quantidade } = req.body;
-
-    const produto = produtos.find(p => p.id === Number(id));
-
-    if (!produto) {
-        return res.status(404).json({ mensagem: 'Produto não encontrado' });
+app.post('/produtos/:id/vender', async (req, res) => {
+    
+    const {id} = req.params;
+    const {quantidade} = req.body;
+    
+    const prod = {
+        id: Number(id),
+        quantidade: Number(quantidade)
     }
 
-    if (produto.quantidade < quantidade) {
-        return res.status(400).json({ mensagem: 'Quantidade insuficiente em estoque' });
+    try {
+        const produto = await venderProduto(prod)
+
+        return res.status(200).json({
+            mensagem: 'Quantidade atualizada',
+            produtos: produto
+        });
+
+    } catch (erro) {
+        if(erro.message === 'NAO_ENCONTRADO'){
+            return res.status(404).json({
+                mensagem: 'Produto não encontrado'
+            })
+        };
+
+        if(erro.message === 'ESTOQUE_INSUFICIENTE'){
+            return res.status(400).json({
+                mensagem: 'Estoque insuficiente'
+            })
+        };
+        return res.status(500).json({ mensagem: 'Erro ao buscar produto' });
     }
-
-    produto.quantidade -= quantidade;
-
-    return res.json({ mensagem: 'Produto vendido com sucesso', produto });
 });
 
 
-app.delete('/remover-produto/:id', (req, res) => {
+app.delete('/remover-produto/:id', async (req, res) => {
     const id = Number(req.params.id);
-    const index = produtos.findIndex(p => p.id === id);
+    
+    try {
+        const produto = await deletaProduto(id);
 
-    if (index === -1) {
-        return res.status(404).json({ mensagem: 'Produto não encontrado' });
+        return res.status(200).json({
+            mensagem: 'Produto Excluido com Sucesso',
+            produto: produto
+        });
+
+    } catch (erro) {
+        console.error(erro);
+        
+        if(erro.message === 'PRODUTO_NAO_ENCONTRADO'){
+            return res.status(404).json({
+                mensagem: 'Produto não foi encontrado'
+            })
+        }
+        return res.status(500).json({ mensagem: 'Erro ao buscar produto' });
     }
-
-    produtos.splice(index, 1);
-
-    return res.json({ mensagem: 'Produto removido com sucesso' });
 });
 
-app.put('/atualizar-produto/:id', (req, res) => {
+app.put('/atualizar-produto/:id', async (req, res) => {
     const id = Number(req.params.id);
     const {nome, preco, quantidade} = req.body;
-    const produto = produtos.find(p => p.id === id);
+    const prod ={
+        id: id,
+        nome: nome ?? null,
+        preco: preco ?? null,
+        quantidade: quantidade ?? null
+    };
+    if (preco != null && isNaN(preco) || preco <= 0) {
+        return res.status(400).json({ mensagem: 'Preço inválido' });
+    }
+     try {
+        const produto = await atualizaProduto(prod)
 
-    if (!produto) {
-        return res.status(404).json({ mensagem: 'Produto não encontrado' });
+        return res.status(200).json({
+            mensagem: 'Produto Atualizado',
+            produto: produto
+        });
+
+    } catch (erro) {
+        if(erro.message === 'NAO_ENCONTRADO'){
+            return res.status(404).json({
+                mensagem: 'Produto Não Encontrado'
+            })
+        }
+        if(erro.message === 'PROD_JA_CADASTRADO'){
+            return res.status(400).json({
+                mensagem: 'Ja existe produto com esse nome'
+            })
+        }
+
+        console.error(erro);
+        return res.status(500).json({ 
+            mensagem: 'Erro Interno' 
+        });
     }
 
-    produto.nome = nome || produto.nome; 
-    produto.preco = preco || produto.preco;
-    produto.quantidade = quantidade || produto.quantidade;    
-    return res.json({ mensagem: 'Produto atualizado com sucesso', produto });
 });
 app.listen(3000, () => {
   console.log('Servidor rodando em http://localhost:3000');
